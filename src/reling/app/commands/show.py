a590d84f5582180c07827b51aa856b7a27613aac
Danylo Mysak <danmysak@gmail.com>
2024-08-14 06:18:10 +0300

    Implement the 'show' command

--- src/reling/app/commands/show.py
+++ src/reling/app/commands/show.py
@@ -1,12 +1,31 @@
 from reling.app.app import app
-from reling.app.types import CONTENT_ARG, LANGUAGE_OPT_ARG
+from reling.app.translation import get_dialog_exchanges, get_text_sentences
+from reling.app.types import API_KEY, CONTENT_ARG, LANGUAGE_OPT_ARG, MODEL
+from reling.db.models import Dialog, Language, Text
+from reling.gpt import GPTClient
 
 __all__ = [
     'show',
 ]
 
+SPEAKER_PREFIX = '> '
+USER_PREFIX = '< '
+
 
 @app.command()
-def show(content: CONTENT_ARG, language: LANGUAGE_OPT_ARG = None) -> None:
+def show(api_key: API_KEY, model: MODEL, content: CONTENT_ARG, language: LANGUAGE_OPT_ARG = None) -> None:
     """Display a text or dialog, or its translation if a language is specified."""
-    pass
+    gpt = GPTClient(api_key=api_key, model=model)
+    (show_text if isinstance(content, Text) else show_dialog)(gpt, content, language or content.language)
+
+
+def show_text(gpt: GPTClient, text: Text, language: Language) -> None:
+    for sentence in get_text_sentences(gpt, text, language):
+        print(sentence)
+
+
+def show_dialog(gpt: GPTClient, dialog: Dialog, language: Language) -> None:
+    exchanges = get_dialog_exchanges(gpt, dialog, language)
+    for exchange in exchanges:
+        print(SPEAKER_PREFIX + exchange.speaker)
+        print(USER_PREFIX + exchange.user)
