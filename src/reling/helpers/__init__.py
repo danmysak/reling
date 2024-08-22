b5dda85bd24abb607dc64357dcafe6f97661180d
Danylo Mysak <danmysak@gmail.com>
2024-08-22 15:27:43 +0300

    Refine helpers description

--- src/reling/helpers/__init__.py
+++ src/reling/helpers/__init__.py
@@ -1,1 +1,1 @@
-# As opposed to utils, helpers import other application modules and implement more complex logic.
+# As opposed to utils, helpers import other application modules and generally implement more specific behavior.
