8e3f6a294c7a5a60e5c29a2bf1573e845b6a78a9
Danylo Mysak <danmysak@gmail.com>
2024-08-25 17:43:07 +0300

    Improve the experience of waiting for key press

--- src/reling/app/commands/exam/presentation.py
+++ src/reling/app/commands/exam/presentation.py
@@ -4,6 +4,7 @@ from typing import Iterable
 from reling.helpers.output import output_text
 from reling.tts import TTSVoiceClient
 from reling.types import DialogueExchangeData
+from reling.utils.console import wait_for_key_press
 from reling.utils.scores import format_average_score
 from reling.utils.transformers import get_numbering_prefix
 from .scoring import MAX_SCORE
@@ -20,10 +21,6 @@ class TitleData:
     text: str
     should_number: bool
     tts: TTSVoiceClient | None
-
-
-def wait_for_key_press() -> None:
-    input()
 
 
 def present_results(
--- src/reling/utils/console.py
+++ src/reling/utils/console.py
@@ -1,7 +1,21 @@
 __all__ = [
     'clear_current_line',
+    'clear_previous_line',
+    'wait_for_key_press',
 ]
+
+KEY_PRESS_PROMPT = '... Press Enter to continue ...'
 
 
 def clear_current_line() -> None:
     print('\033[K', end='\r')
+
+
+def clear_previous_line() -> None:
+    print('\033[F\033[K', end='\r')
+
+
+def wait_for_key_press() -> None:
+    print('')
+    input(KEY_PRESS_PROMPT)
+    clear_previous_line()
